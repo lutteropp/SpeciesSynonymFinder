6d78690774d792bf8cbbd60766da6218eefaa113
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2017-12-18 14:56:45 +0100

    added the script

---
+++ find_synonyms.r
@@ -0,0 +1,28 @@
+library(jsonlite)
+
+findSynonyms <- function(species_name) {
+  synonyms = c("")
+  species_name_no_spaces = gsub(" ","+",species_name, fixed=TRUE)
+  query = paste("http://webservice.catalogueoflife.org/col/webservice?name=", species_name_no_spaces, "&format=json&response=full", sep = "")
+  dbentry = fromJSON(query, flatten=TRUE)
+  if (length(dbentry$results) == 0) {
+    print(paste("Unfortunately, no results were found for ", species_name, ". Please check your spelling."), sep="")
+  } else {
+    for (i in 1:length(dbentry$results)) {
+      name = paste(dbentry$results[i,]$name)
+      if (name == species_name) {
+        status = paste(dbentry$results[i,]$name_status)
+        if (status == "synonym") {
+          #print(paste("Adding", dbentry$results[i,]$accepted_name.name))
+          synonyms = rbind(synonyms,c(paste(dbentry$results[i,]$accepted_name.name)))
+        } else { #accepted name
+          for (j in 1:length(dbentry$results[i,]$synonyms[[1]]$name)) {
+            #print(paste("Adding", dbentry$results[i,]$synonyms[[1]]$name[j]))
+            synonyms = rbind(synonyms,c(paste(dbentry$results[i,]$synonyms[[1]]$name[j])))
+          }
+        }
+      }
+    }
+  }
+  return(synonyms)
+}
